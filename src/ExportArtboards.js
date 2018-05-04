7313b1862190193b5b7ec21851c82f42c3f699a2
Dory Azar <dory.azar@gmail.com>
2018-05-04 13:43:33 -0400

    Changing supported formats on the Export artboards plugin

--- src/ExportArtboards.js
+++ src/ExportArtboards.js
@@ -11,7 +11,7 @@ export default function(context) {
     var mysketch = new Component(context);
 
     var scales = sketch.UI.getStringFromUser('Enter image scales separated by comas', 'example: 1, 2, 3');
-    var formats = sketch.UI.getStringFromUser('Enter image formats separated by comas', 'example: png, jpeg, jpg');
+    var formats = sketch.UI.getStringFromUser('Enter image formats separated by comas', 'example: png, jpg, pdf, svg');
     mysketch.exportArtboards(scales, formats);
 
 }
